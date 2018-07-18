da391f716101cb6ff23179d02966f5479fd72730
Daniele Sangalli <sangallidaniele@gmail.com>
2018-07-18 16:00:13 +0100

    WIP

--- src/etl.js
+++ src/etl.js
@@ -1,16 +1,44 @@
 "use strict"
 
-const {BUCKET, ROLE, CRONJOB} = process.env
+const {BUCKET, ROLE} = process.env
 
-const {check_manifest} = require("./s3")
+const {check_manifest, get_job_type} = require("./s3")
 const {start_kube_job} = require("./kube")
 
 const sqs_message_handler = async (message, done) => {
-  if (await check_manifest(BUCKET)) await start_kube_job(ROLE, CRONJOB)
-  else console.info("Files don't yet match the manifest")
+  if (is_manifest(message)) {
+    if (await check_manifest(BUCKET)) {
+      const incrementalFilePath = get_manifest_path(message) + "incremental"
+      const jobType = await get_job_type(incrementalFilePath)
+
+      if (jobType === "bulk") {
+        await start_kube_job(ROLE, "neo4j-bulk")
+        await start_kube_job(ROLE, "elastic-bulk")
+
+      } else if (jobType === "delta") {
+        await start_kube_job(ROLE, "neo4j-delta")
+        await start_kube_job(ROLE, "elastic-delta")
+      } else {
+        console.error("jobType wasnt captured correctly")
+      }
+    } else {
+      console.info("Files don't yet match the manifest")
+    }
+  }
+
   done()
 }
 
+const is_manifest = a =>
+  JSON.parse(a.Body).Records[0].s3.object.key.indexOf("manifest") > -1
+    ? true
+    : false
+
+const get_manifest_path = a =>
+  JSON.parse(a.Body).Records[0].s3.object.key.split("/manifest.json")[0]
+
 module.exports = {
-  sqs_message_handler
+  sqs_message_handler,
+  is_manifest,
+  get_manifest_path
 }
--- src/s3.js
+++ src/s3.js
@@ -1,9 +1,7 @@
 "use strict"
 
 const {S3_ACCESS_KEY, S3_SECRET_KEY, REGION} = process.env
-
 const AWS = require("aws-sdk")
-
 const client = new AWS.S3({
   accessKeyId: S3_ACCESS_KEY,
   secretAccessKey: S3_SECRET_KEY,
@@ -15,7 +13,7 @@ const get_manifest = async (bucket, manifest_file = "manifest.json") =>
     .getObject({Bucket: bucket, Key: manifest_file})
     .promise()
     .then(object => ({
-      data: JSON.parse(object.Body.toString('utf8'))
+      data: JSON.parse(object.Body.toString("utf8"))
     }))
 
 const get_object_hash = async (bucket, key) =>
@@ -35,6 +33,18 @@ const check_manifest = async bucket => {
 
   return !res.includes(false)
 }
+
+const get_job_type = async (bucket, key) =>
+  client
+    .headObject({Bucket: bucket, Key: key})
+    .promise()
+    .then((err, data) => {
+      const jobType = key.indexOf("incremental") > -1 ? "delta" : "bulk"
+      if (err && err.code === "NotFound") {
+        return jobType == "delta" ? "bulk" : "delta"
+      }
+      return jobType
+    })
 
 module.exports = {
   get_manifest,
