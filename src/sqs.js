bc7d860b70c498e28e56ebf8bee54e10dde18989
Rob Dabin <robert.dabin@digital.homeoffice.gov.uk>
2018-07-31 16:21:24 +0100

    adds missing environment variable for SQS

--- src/sqs.js
+++ src/sqs.js
@@ -1,15 +1,15 @@
 "use strict"
 
-const {SQS_ACCESS_KEY, SQS_SECRET_KEY, REGION} = process.env
+const {SQS_ACCESS_KEY, SQS_SECRET_KEY, REGION, QUEUE} = process.env;
 
-const AWS = require("aws-sdk")
-const sqs = require("sqs-consumer")
+const AWS = require("aws-sdk");
+const sqs = require("sqs-consumer");
 
 const client = new AWS.SQS({
   accessKeyId: SQS_ACCESS_KEY,
   secretAccessKey: SQS_SECRET_KEY,
   region: REGION
-})
+});
 
 const createConsumer = handler =>
   sqs.create({
