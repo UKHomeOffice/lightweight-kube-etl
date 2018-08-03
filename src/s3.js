5fa2e0201f727e6358ab87863675ddc3057e20d0
Daniele Sangalli <sangallidaniele@gmail.com>
2018-08-03 15:28:39 +0100

    -testing stuff

--- src/s3.js
+++ src/s3.js
@@ -1,43 +1,43 @@
 "use strict"
 
-const AWS = require("aws-sdk");
-const R = require("ramda");
-const Promise = require("bluebird");
+const AWS = require("aws-sdk")
+const R = require("ramda")
+const Promise = require("bluebird")
 
 const {S3_ACCESS_KEY, S3_SECRET_KEY, REGION} = process.env,
-    client = Promise.promisifyAll(new AWS.S3({
-        accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY, region: REGION
-    }));
+  client = Promise.promisifyAll(
+    new AWS.S3({
+      accessKeyId: S3_ACCESS_KEY,
+      secretAccessKey: S3_SECRET_KEY,
+      region: REGION
+    })
+  )
 
 const getManifest = async (bucket, manifest_file = "manifest.json") => {
-
-    return client.getObjectAsync({ Bucket: bucket, Key: manifest_file })
-        .then(object => ({ data: JSON.parse(object.Body.toString("utf8")) }));
-
-};
+  return client
+    .getObjectAsync({Bucket: bucket, Key: manifest_file})
+    .then(object => ({data: JSON.parse(object.Body.toString("utf8"))}))
+}
 
 const getObjectHash = async (bucket, key) => {
-
-    return client.headObjectAsync({ Bucket: bucket, Key: key })
-        .then(object => object.ETag);
-
-};
+  return client
+    .headObjectAsync({Bucket: bucket, Key: key})
+    .then(object => object.ETag)
+}
 
 function checkManifest(bucket, manifestFilePath) {
-
-    return getManifest(bucket, manifestFilePath).then((manifest) => {
-
-        return Promise.all(R.map(isHashOk(bucket), manifest.data))
-            .then(R.all(R.equals(true)));
-
-    });
+  return getManifest(bucket, manifestFilePath).then(manifest => {
+    return Promise.all(R.map(isHashOk(bucket), manifest.data)).then(
+      R.all(R.equals(true))
+    )
+  })
 }
 
 const isHashOk = R.curry((bucket, fileMetadata) => {
-
-    return getObjectHash(bucket, fileMetadata.FileName)
-        .then((hash) => R.equals(fileMetadata.SHA256, hash));
-});
+  return getObjectHash(bucket, fileMetadata.FileName).then(hash =>
+    R.equals(fileMetadata.SHA256, hash)
+  )
+})
 
 const getJobType = async (bucket, key) => {
   client
@@ -52,25 +52,26 @@ const getJobType = async (bucket, key) => {
     })
 }
 
-const getJobTypeFromPath = (path) => {
+const getJobTypeFromPath = path => {
+  const jobType = R.head(R.last(path.split("/")).split("."))
 
-    const jobType = R.head(R.last(path.split("/")).split("."));
+  if (!jobType) {
+    console.error("jobType was not captured correctly")
 
-    if (!jobType) {
+    throw new Error("jobType was not captured correctly")
+  }
 
-        console.error("jobType was not captured correctly");
+  return jobType
+}
 
-        throw new Error("jobType was not captured correctly");
-    }
-
-    return jobType;
-};
-
-const isTxtFileObject = R.compose(R.test(/.txt$/), R.path(["Key"]));
-
+const isTxtFileObject = R.compose(
+  R.test(/.txt$/),
+  R.path(["Key"])
+)
 
 module.exports = {
-    getObjectHash,
-    checkManifest,
-    getJobType
-};
+  getManifest,
+  getObjectHash,
+  checkManifest,
+  getJobType
+}
