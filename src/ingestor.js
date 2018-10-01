51e76d4eb27dd2c117fd91e546a2b5ffecaae378
Bernard Mordan <bernard.mordan@digital.homeoffice.gov.uk>
2018-10-01 12:08:43 +0100

    better fetching from s3

--- src/ingestor.js
+++ src/ingestor.js
@@ -1,13 +1,14 @@
 const R = require('ramda');
 const moment = require('moment');
-const pollingInterval = 1 * 60 * 1000; // 1 minuet
+const pollingInterval = 1000; // 1 minuet
+const { BUCKET: Bucket } = process.env;
 
 const isTimestamp = label => moment.unix(label).isValid();
 
 const hasTimestampFolders = R.compose(
   R.any(isTimestamp),
   R.map(R.compose(R.head, R.tail, R.split('/'), R.prop('Key'))),
-  R.prop('Content')
+  R.prop('Contents')
 );
 
 const getIngestJobParams = R.compose(
@@ -19,17 +20,25 @@ const getIngestJobParams = R.compose(
   R.filter(R.compose(R.contains(R.__, ["bulk.txt", "incremental.txt"]), R.last)),
   R.map(R.take(3)),
   R.map(R.compose(R.split("/"), R.prop("Key"))),
-  R.prop('Content')
+  R.prop('Contents')
 );
 
 function control_loop (s3, mongodb, kubectl) {
-  s3.listObjectsV2({Prefix: "pending/", Delimiter: ""}, (err, folder) => {
-    if (!folder) {
-      setTimeout(control_loop, pollingInterval);
-    } else if (!folder.Contents.length) {
-      setTimeout(control_loop, pollingInterval);
+  s3.listObjectsV2({Bucket, Prefix: "pending/", Delimiter: ""}, (err, folder) => {
+    console.log(folder);
+    if (err) {
+      console.error(JSON.stringify(err, null, 2));
+      
+      return setTimeout(control_loop, pollingInterval);
+
+    } else if (!folder || !folder.Contents.length) {
+      
+      return setTimeout(control_loop, pollingInterval);
+
     } else if (!hasTimestampFolders(folder)) {
-      setTimeout(control_loop, pollingInterval);
+      
+      return setTimeout(control_loop, pollingInterval);
+
     } else {
       const ingestParams = getIngestJobParams(folder);
 
@@ -42,7 +51,7 @@ function waitForManifest (s3, ingestParams, kubectl) {
   const { ingestName } = ingestParams;
   const manifestPrefix = `pending/${ingestName}/manifest.json`;
 
-  s3.listObjectsV2({Prefix: manifestPrefix, Delimiter: ""}, (err, {Contents}) => {
+  s3.listObjectsV2({Bucket, Prefix: manifestPrefix, Delimiter: ""}, (err, {Contents}) => {
     return !Contents.length
       ? setTimeout(() => waitForManifest(s3, ingestParams, kubectl), pollingInterval)
       : triggerIngest(ingestParams, kubectl);
