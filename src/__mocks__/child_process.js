cbc844095f047d5b6e1d5d9faee8dfd11731a07b
Bernard Mordan <bernard.mordan@digital.homeoffice.gov.uk>
2018-10-23 16:28:27 +0100

    70% create bulk jobs

--- src/__mocks__/child_process.js
+++ src/__mocks__/child_process.js
@@ -52,9 +52,10 @@ const get_job_status = jest.fn()
   .mockReturnValueOnce(running_job)
   .mockReturnValue(complete_job);
 
-const get_run_job = jest.fn()
+const get_exit_code_1_0 = jest.fn()
   .mockReturnValueOnce(1)
-  .mockReturnValueOnce(0)
+  .mockReturnValue(0)
+
 
 function getOutput (command) {
   const cmd = command.replace("kubectl --context acp-notprod_DACC -n dacc-entitysearch --token MOCK_TOKEN ", "")
@@ -65,10 +66,28 @@ function getOutput (command) {
       return {stdout: get_pod_status(), stderr: null};
     case "get pods neo4j-1 -o json":
       return {stdout: get_pod_status(), stderr: null};
+    case "get pods elasticsearch-0 -o json":
+      return {stdout: pod_status_ready, stderr: null};
+    case "get pods elasticsearch-1 -o json":
+      return {stdout: pod_status_ready, stderr: null};
     case "get jobs neo4j-delta-1538055240 -o json":
       return {stdout: get_job_status(), stderr: null};
+    case "get jobs neo4j-bulk-1538055555 -o json":
+      return {stdout: complete_job, stderr: null}
+    case "get jobs elastic-bulk-1538055555 -o json":
+      return {stdout: complete_job, stderr: null}
+    case "create job neo4j-bulk-1538055555 --from cronjob/neo4j-bulk":
+      return { exitcode: get_exit_code_1_0() };
+    case "create job elastic-bulk-1538055555 --from cronjob/elastic-bulk":
+      return { exitcode: 0 };
+    case "create job neo4j-delta-1538022222 --from cronjob/neo4j-delta":
+      return { exitcode: 1 };
     case "create job neo4j-delta-1538055240 --from cronjob/neo4j-delta":
-      return {exitcode: get_run_job() };
+      return { exitcode: 0 };
+    case "create job elastic-delta-1538055240 --from cronjob/elastic-delta":
+      return { exitcode: 0 };
+    default:
+      return { exitcode: 0 };
   }
 }
 
@@ -84,7 +103,7 @@ function exec (command, callback) {
 
 function spawn (cmd, cmd_args) {
   const _spawn = new Spawn();
-  const {exitcode} = getOutput(cmd + ' ' + cmd_args.join(' '));
+  const { exitcode } = getOutput(cmd + ' ' + cmd_args.join(' '));
   setTimeout(() => _spawn.emit('exit', exitcode), 10);
   return _spawn;
 }
