edd653b7480c22ed8bdaad3a745bae91c8f2f6d6
Daniele Sangalli <daniele.sangalli@digital.homeoffice.gov.uk>
2020-08-14 15:29:37 +0100

    updated libs. fixed outdated libs. fixed drone build. Fixed issue on date comparison. added prettier. Fixed Wallaby

--- src/s3-client.spec.js
+++ src/s3-client.spec.js
@@ -1,24 +1,24 @@
-jest.mock('./s3-client');
+jest.mock("./s3-client");
 
-const { start, waitForManifest } = require('./ingestor');
+const { start, waitForManifest } = require("./ingestor");
 
-const s3 = require('./s3-client');
+const s3 = require("./s3-client");
 
-describe('The s3 client', () => {
-  it('will keep looking in an AWS bucket if it errors, is empty, has no timestamped folder, has no manifest file', done => {
+describe("The s3 client", () => {
+  it("will keep looking in an AWS bucket if it errors, is empty, has no timestamped folder, has no manifest file", done => {
     start(ingestParams => {
-      expect(ingestParams).toEqual({ingestName: '1538055240', ingestType: 'bulk'});
+      expect(ingestParams).toEqual({ ingestName: "1538055240", ingestType: "bulk" });
       expect(s3.listObjectsV2.mock.calls.length).toBe(6);
       done();
     });
   });
 
-  it('will wait for a manifest file', done => {
-    const ingestParams = {ingestName: '1538055240', ingestType: 'bulk'};
-    
+  it("will wait for a manifest file", done => {
+    const ingestParams = { ingestName: "1538055240", ingestType: "bulk" };
+
     waitForManifest(ingestParams, _ingestParams => {
       expect(ingestParams).toEqual(_ingestParams);
       done();
     });
   });
-})
+});
